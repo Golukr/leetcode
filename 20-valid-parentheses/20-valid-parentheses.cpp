73b6b99ad27a07080b58f11b6d34f9948062f6ca
Golu Kumar <89934654+Golukr@users.noreply.github.com>
2022-07-03 18:29:21 +0530

    Time: 7 ms (7.56%), Space: 6.2 MB (96.03%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(int i=0; i<s.length(); i++)
+        {
+            char ch = s[i];
+            //if openning bracket, stack push
+            //if closing bracket, stacktop check and top
+            if(ch=='(' || ch=='{' || ch=='[')
+            {
+                st.push(ch);
+            }
+            else{
+                //for closing bracket
+                if(!st.empty())
+                {
+                    char top=st.top();
+                    if(ch==')' && top=='(' || ch=='}' && top=='{' || ch==']' && top=='[')
+                    {
+                        st.pop();
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else{
+                    return false;
+}
+            }
+        }
+        if(st.empty())
+            return true;
+        else
+            return false;
+    }
+};
