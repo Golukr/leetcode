30133a4d3c83513e92b93521615ec5f5f0e79daa
Golu Kumar <89934654+Golukr@users.noreply.github.com>
2022-07-14 20:33:46 +0530

    Time: 35 ms (15.60%), Space: 7.8 MB (42.21%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -4,8 +4,8 @@ public:
 	unordered_map<char, int> mp = {{'M', 1000}, {'D', 500}, {'C', 100}, {'L', 50}, {'X', 10}, {'V', 5}, {'I', 1}};
 	int res = mp[s.back()]; // store value of last char of ip string
 	for(int i = 0; i < s.size() - 1; i++) { // iterate through all the character
-		if(mp[s[i]] < mp[s[i + 1]]) res -= mp[s[i]];
-		else res += mp[s[i]];
+		if(mp[s[i]] < mp[s[i + 1]]) res -= mp[s[i]]; //subtract if prev character's value is less than curr char
+		else res += mp[s[i]]; // else add
 	}
 	return res;
 
