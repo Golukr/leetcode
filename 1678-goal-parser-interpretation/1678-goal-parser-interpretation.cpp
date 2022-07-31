8366d2541d1a5d75c3f1a16543f9d03ab8f45f08
Golu Kumar <89934654+Golukr@users.noreply.github.com>
2022-07-31 09:17:36 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (55.60%) - LeetHub

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string interpret(string s) {
+        string ans;
+        for(int i=0; i<s.length(); )
+        {
+            if(s[i]=='G') ans += "G", ++i;
+            else if(s[i+1]==')') ans += "o", i+=2;
+            else
+                ans += "al", i+=4;
+        }
+        return ans;
+    }
+};
