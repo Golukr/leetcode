2af7a94ce4abdf473d86026cf91e971160b8245c
Golu Kumar <89934654+Golukr@users.noreply.github.com>
2023-09-30 09:36:08 +0530

    Added solution - LeetHub

---
+++ Fibonacci Expression - GFG/fibonacci-expression.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    int fibExpression(int N){
+        // code here
+        if(N%2==0){
+            return 1;
+        }else{
+            return -1;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        
+        Solution ob;
+        cout<<ob.fibExpression(N)<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
