037bd8874dceeef93d6df09a9d0fd2d3588c1aff
Golu Kumar <89934654+Golukr@users.noreply.github.com>
2023-10-06 06:57:41 +0530

    Added solution - LeetHub

---
+++ Remove characters from alphanumeric string - GFG/remove-characters-from-alphanumeric-string.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+ 
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+
+	string removeCharacters(string S) 
+	{
+	    // Your code goes here
+	   string ans="";
+	   for(int i=0; i<S.length(); i++){
+	       if(isdigit(S[i])){
+	           ans+=S[i];
+	       }
+	   }
+	   return ans;
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{
+   	
+
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+   	int t;
+   	cin >> t;
+   	while(t--)
+   	{
+   		string s;
+   		cin >> s;
+   		Solution ob;
+   		cout << ob.removeCharacters(s) << "\n";
+   		
+   	}
+
+    return 0;
+}
+// } Driver Code Ends
